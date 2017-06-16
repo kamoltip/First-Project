e0ce5264ee1514c4b994a28caea6a0b588e3ca02
jliip51 <jliip51@gmail.com>
2017-06-16 18:40:30 -0400

    Added wireframe and firebase

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,3 +1,14 @@
-$(body).on("click", function(){
+$(document).on("click", function(){
 	alert("GUYS LET'S MAKE OUR APP ALIVE!. WE CAN DO IT!!")
 });
+
+var config = {
+  apiKey: "AIzaSyDxW087mUoLk6smGAHixRd5lLKYBZ4JeA8",
+  authDomain: "first-project-5b478.firebaseapp.com",
+  databaseURL: "https://first-project-5b478.firebaseio.com",
+  projectId: "first-project-5b478",
+  storageBucket: "",
+  messagingSenderId: "618602692351"
+};
+
+firebase.initializeApp(config);
