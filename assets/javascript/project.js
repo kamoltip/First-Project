fa5377aabf0d539f1063b3055daeccaa061e2738
Moss Goggins <myrealflorida@gmail.com>
2017-06-19 01:51:21 -0400

    youtube search 1st commit

---
+++ assets/javascript/project.js
@@ -0,0 +1,32 @@
+  var searchTopic= 'atlanta';
+  var order = 'date';
+  var videoID;
+  var queryURL = 'https://www.googleapis.com/youtube/v3/search?maxResults=5&part=snippet&q='+searchTopic+'&order='+order+'&type=video&videoEmbeddable=true&key=AIzaSyCnbcvaas-tjIurM5-936c9S3mT5dJgTIo';
+  $.ajax({
+    url:queryURL,
+    method:'GET',
+    dataType: 'jsonp'
+  })
+
+  .done(function(response){
+  console.log(response);
+  console.log(response.items);
+
+    // var results = data.items;
+    for (var i = 0; i < response.items.length; i++) {
+      var youtubeDiv = $("<iframe class='youtube' allowfullscreen>");
+      youtubeDiv.css({"width": "200px", "height": "140px"});
+      var videoIdList = response.items[i].id.videoId;
+      var url = 'https://www.youtube.com/embed/' + videoIdList;
+      console.log(url);
+      // grabbing the title for every video
+      var videoTitle = response.items[i].snippet.title;
+      console.log(videoTitle);
+      youtubeDiv.attr("src", url);
+      $("#displayedVideo").append(youtubeDiv);
+      }
+  })
+
+  .fail(function(err){
+    console.log(err.statusText);
+  })
